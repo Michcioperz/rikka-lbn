9350bb18596c761ec5b405ef28efc905d31ba67e
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2015-10-24 13:06:42 +0200

    Newport

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var schedule = require('node-schedule');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-server.listen(5000);
+server.listen(5004);
 
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
@@ -22,8 +22,10 @@ MongoClient.connect('mongodb://localhost:27017/rikka', function(err, mon) {
 io.on('connection', function(socket) {
   db.collection('users').insertOne({"id": socket.id, "nickname": "Hero", "location": {"type": "Point", "coordinates": [51.2493, 22.5367]}, "health": 1});
   socket.on('registration', function (data) {
+    db.collection('users').updateOne({"id": socket.id},{$set: {"nickname": data.nickname}}, function (err, results) { console.log("Registration complete"); });
   });
   socket.on('locupdate', function (data) {
+    db.collection('users').updateOne({"id": socket.id},{$set: {"location.coordinates":[data.latitude, data.longitude]}}, function (err, results) { console.log("Registration complete"); });
   });
   socket.on('hit', function (data) {
   });
